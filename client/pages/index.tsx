ad3ea38bf60ea948d1a177d0c07598cbdfeebbdf
suyeon <suyeon.dv@gmail.com>
2022-06-06 19:41:04 +0900

    [FEAT] client 측 세팅

---
+++ client/pages/index.tsx
@@ -0,0 +1,17 @@
+import styled from '@emotion/styled';
+
+const Wrapper = styled.div``;
+
+const Styled = { Wrapper };
+
+const Home = () => {
+    return (
+        <Styled.Wrapper>
+            HOME 아.... 세팅 힘들다.....퓨
+            아니... 바벨 안 써도 되는데... SWC 있는데....
+            왜... 워닝 뭐냐고... emotion 뭐냐고... 아.......슬프다아아아아아아아아아
+        </Styled.Wrapper>
+    )
+}
+
+export default Home;
